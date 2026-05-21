0755e89c4ebfffad1ecb4b530b753b6979ef0631
Kagan-Yuri <dh590350@gmail.com>
2026-05-21 15:00:27 +0700

    render

---
+++ my-app-su26-ex/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from './components/Home';
+import Gallery from './components/Gallery';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
