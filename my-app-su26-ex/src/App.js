57e8a6e44581544bcf7ea2c80f801208b3b57030
Kagan-Yuri <dh590350@gmail.com>
2026-06-01 13:40:16 +0700

    Update App.js

--- my-app-su26-ex/src/App.js
+++ my-app-su26-ex/src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import Home from './components/Home';
-import Gallery from './components/Gallery';
-
+import React from "react";
+// import Home from './components/Home';
+// import Gallery from './components/Gallery';
+// import SideBarAndTable from './components/SideBarAndTable';
+// import { Container } from 'react-bootstrap';
+// import Header from './components/Header';
+// import Movies from './components/Movies';
+import Books from "./components/Books";
+import "bootstrap/dist/css/bootstrap.min.css";
+// Import các component bài tập đã làm ở bước trước
+// import Tab_S17 from './components/Tab_S17';
+// import Modal_S20 from './components/Modal_S20';
+// import Alert_S21 from './components/Alert_S21';
+// import Card_S22 from './components/Card_S22';
+// import UseState from './components/UseState';
+// import UseProp from './components/UseProp';
+// import Hello from './components/Hello';
+// import Paging from "./components/Paging";
+// import Avatar from './components/Avatar';
+// import { items } from './Data';
+import StudentTable from "./components/StudentTable";
+// import UseEffect from './components/UseEffect';
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Gallery />
+      {/* 
+      <UseProp width="200px" height="100px" color="#fedede" title="My Box" />
+      <UseProp/>
+      <Hello who="Alice" age={30} /> */}
+      {/* <Paging /> */}
+      {/* { items?.map((t) => (<Avatar name={t.name} gender={t.gender} image={t.image} />)) } */}
+      <StudentTable />
+      {/* <UseEffect /> */}
+      <Books />
     </div>
   );
 }
