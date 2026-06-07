89f2e4433d303f803a35717724075bb84fc0d46c
Kagan-Yuri <dh590350@gmail.com>
2026-06-07 09:02:03 +0700

    Update navigate on Movie

--- my-app-su26-ex/src/components/Movies.jsx
+++ my-app-su26-ex/src/components/Movies.jsx
@@ -1,35 +1,55 @@
-import React from "react";
+import { React, useState } from "react";
 import { movies } from "../Data";
-import { Button, Col, Card } from "react-bootstrap";
+import { Button, Col, Card, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function Movies() {
+  const [selectedM,setSelectedM] = useState(null); 
+  const navigate = useNavigate();
+  const handleViewShowtime = (movieId =>
+  navigate(`/view-showtime/${movieId}`));
+  const handleSelected = (m) => {
+    setSelectedM(m);
+  };
+
   return (
-    <div>
+    <Row>
       <h2
         style={{ textAlign: "center", marginBottom: "20px", fontSize: "2rem" }}
       >
         Movies
       </h2>
-      <div className="d-flex flex-wrap justify-content-center">
-        {movies.map((movie) => (
-          <Col key={movie.id} md={4} className="mb-4">
+      <Row className="d-flex flex-wrap justify-content-center">
+        {movies?.map((m => (
+          <Col key={m.id} md={4} className="mb-4">
             <Card
-              key={movie.id}
+              key={m.id}
               className="card m-2"
               style={{ width: "18rem" }}
             >
               <Card.Body>
-                <Card.Title>{movie.title}</Card.Title>
-                <Card.Text>Duration: {movie.duration} mins</Card.Text>
-                <Card.Text>Director: {movie.director}</Card.Text>
-                <Card.Text>Rating: {movie.rating}</Card.Text>
-                <Button className="btn btn-primary">View Showtime</Button>
+                <Card.Title>{m.title}</Card.Title>
+                <Card.Text>Duration: {m.duration} mins</Card.Text>
+                <Card.Text>Director: {m.director}</Card.Text>
+                <Card.Text>Rating: {m.rating}</Card.Text>
+                <Button onClick ={() => handleSelected(m)}> To View Showtime </Button>
+                <Button className="btn btn-primary" onClick={() => handleViewShowtime(m.id)}>View Showtime</Button>
               </Card.Body>
             </Card>
           </Col>
-        ))}
-      </div>
-    </div>
+        )))}
+      </Row>
+      { selectedM && (
+        <Card className="mt-3">
+          <Card.Body>
+            <Card.Title>{selectedM.title}</Card.Title>
+            <Card.Text>Duration: {selectedM.duration} mins</Card.Text>
+            <Card.Text>Director: {selectedM.director}</Card.Text>
+            <Card.Text>Rating: {selectedM.rating}</Card.Text>
+          </Card.Body>
+        </Card>
+      )}
+    </Row>
   );
 }
 
