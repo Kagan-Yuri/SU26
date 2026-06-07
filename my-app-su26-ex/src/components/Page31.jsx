8b18f34723029e42e21477a359822cdf6b0af5f2
Kagan-Yuri <dh590350@gmail.com>
2026-06-07 20:37:11 +0700

    Page31 components

---
+++ my-app-su26-ex/src/components/Page31.jsx
@@ -0,0 +1,13 @@
+import { React, useState, useEffect } from 'react';
+import { Container, Card, Row, Col, Button, Form } from 'react-bootstrap';
+import { useNavigate, useParam } from 'react-router-dom';
+
+function Page31(){
+    return (
+        <Container>
+            
+        </Container>
+    );
+}
+
+export default Page31;
